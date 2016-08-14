f04f483c2a9dfed8bdeb943e39d121297e4684b0
Evan B <ebensh@gmail.com>
2016-08-14 18:07:05 -0400

    Basic get_scores.sh

---
+++ neverdrains/get_scores.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This file is a suuuper klugey example of curl + awk/sed/grep/tr to process
+# a raw text file into csv format.
+url='https://www.neverdrains.com/pinfest2016/playerIndex.php?disp=game&sel=Classics&game=130'
+curl $url > /var/tmp/neverdrains.html
+
+egrep '(\<tr\>|\<td\>)' /var/tmp/neverdrains.html | \
+    tr '\t' ' ' | \
+    sed 's_^ *__' | \
+    sed 's_</*a[^>]*>__g' | \
+    tr -d '\n' | \
+    sed 's_</tr>_\n_g' | \
+    tr -d ',' | \
+    sed 's_</td>_,_g' | \
+    sed 's_<[^>]*>__g' | \
+    sed 's_,$__' > /var/tmp/scores.csv
+  
+
+
+  
