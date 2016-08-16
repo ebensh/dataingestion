e4a8641a13111ad5d1c17e66804eb78f15d202c2
Evan B <ebensh@gmail.com>
2016-08-15 20:30:20 -0400

    Updated dump_to_parse_to_graph, as well as .gitignore

--- ifpapinball/dump_to_parse_to_graph.sh
+++ ifpapinball/dump_to_parse_to_graph.sh
@@ -3,10 +3,11 @@ set -e
 set -x
 
 python dump_user_history.py api_key 1 | python parse_user_history.py > score_history.csv
-Rscript <(cat <<EOF
-data <- read.csv('score_history.csv', header=TRUE);
-png('score_history.png', height=800, width=600);
-plot(data$rating_date, data$rating);
-dev.off();
+
+cat <<'EOF' | R --no-save
+data <- read.csv('score_history.csv', header=TRUE)
+data$rating_date <- as.Date(data$rating_date)
+png('score_history.png', height=800, width=600)
+plot(data$rating_date, data$rating)
+dev.off()
 EOF
-)
